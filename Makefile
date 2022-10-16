65ce7f876343e2f938e7bfb24621353dbbea09e9
Frank Ockerman <frank.ockerman@gmail.com>
2022-10-15 21:45:46 -0400

    Add data cleaning step

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
 PHONY : clean
 PHONY : getData
+PHONY : cleanData
 
+# "reset" project
 clean :
 	rm -rf sourceData/*
 	rm -rf interData/*
@@ -11,8 +13,8 @@ clean :
 	mkdir -p interData
 	touch .created-dirs
 
-# phony target which depends on source data
-getData : src/getData.sh .created-dirs \
+# phony target used to get source data
+getData : src/getData.sh .created-dirs\
 	sourceData/neg.txt \
 	sourceData/neg_metadata.txt \
 	sourceData/polar.txt \
@@ -34,3 +36,22 @@ sourceData/neg.txt \
 	sourceData/pos_polar_metadata.txt \
 	sourceData/study_design.txt :
 		bash src/getData.sh
+
+# phony target used to clean data
+cleanData : R/cleanData.R .created-dirs \
+	interData/data.tsv \
+	interData/anno.tsv
+
+# cleans and combines source metabolomic data
+# combines annotation data
+# use cleanData target when running make
+interData/data.tsv interData/anno.tsv : \
+	sourceData/neg.txt \
+	sourceData/neg_metadata.txt \
+	sourceData/polar.txt \
+	sourceData/polar_metadata.txt \
+	sourceData/pos_early.txt \
+	sourceData/pos_early_metadata.txt \
+	sourceData/pos_late.txt \
+	sourceData/pos_polar_metadata.txt
+		Rscript R/cleanData.R
---
+++ R/cleanData.R
@@ -0,0 +1,35 @@
+library(tidyverse)
+library(magrittr)
+
+assays <- c("neg", "polar", "pos_early")
+
+readData <- function(assay){
+  dfFile <- paste0("sourceData/", assay, ".txt")
+  ID <- read.table(dfFile, nrows=1) %>% unlist()
+  dfNames <- c("Metabolite", ID)
+  df <- read_tsv(dfFile, col_names = dfNames, skip = 1, col_types = cols())
+  Metabolite <- df$Metabolite
+  df %<>% select(all_of(2:ncol(df))) %>% t() %>%
+    as_tibble(.name_repair = "minimal") %>% setNames(Metabolite)
+  df %<>% mutate(ID = ID)
+  return(df)
+}
+
+readAnno <- function(assay){
+  dfFile <- paste0("sourceData/", assay, "_metadata.txt")
+  dfNames <- c("metabolite_name", "SUPER.PATHWAY", "SUB.PATHWAY",
+               "PLATFORM", "RI", "MASS", "PUBCHEM",
+                "CAS", "KEGG", "HMDB")
+  df <- read_tsv(dfFile, col_names = dfNames, col_types = cols())
+  df %<>% mutate(Assay = assay)
+  return(df)
+}
+
+data <- lapply(assays, function(assay) readData(assay)) %>%
+  Reduce(function(df1, df2) left_join(df1, df2, by="ID"), .)
+
+anno <- lapply(assays, function(assay) readAnno(assay)) %>%
+  do.call("rbind", .)
+
+write_tsv(data, "interData/data.tsv")
+write_tsv(anno, "interData/anno.tsv")
